e0d1a18f040f498967b231dafff08699f9ff5d0d
henry-hieunguyen <84856648+henry-hieunguyen@users.noreply.github.com>
2021-06-17 15:26:49 +0700

    Add files via upload

---
+++ Homework 7 - Q1,2,3.js
@@ -0,0 +1,17 @@
+// Question 1 ||  1 - d   |   2 - c  |  3 - a  | 4 - b
+
+
+
+// Question 2 || 
+let userInput = prompt("Input");
+const printInput = () => {
+    console.log(userInput);
+}
+
+setInterval(printInput, 3000);
+
+
+
+// Question 3 ||
+// a.Can a variable declared by let, in a function still exists after the function finishes running? NO
+// b.Can var variable declared by var, in a function still exists after the function finishes running? NO
